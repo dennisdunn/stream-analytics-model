188421cb14795468fa2bc02c2a534cccd1d8293d
Dennis Dunn <ansofive@gmail.com>
2018-04-01 21:22:57 -0400

    Colors for the stream nodes, dataset picker.

--- src/nodes/utilization.js
+++ src/nodes/utilization.js
@@ -2,15 +2,11 @@ module.exports = function (RED) {
     function Utilization(config) {
         RED.nodes.createNode(this, config);
         this.threshold = config.threshold;
+        this.name = config.name;
         var node = this;
         node.on('input', msg => {
             var context = node.context();
-            var state = context.get('state') || {
-                ishigh: false,
-                prev: 0,
-                total: 0,
-                on: 0
-            };
+            var state = context.get('state') || { prev: null, total: 1, on: 0, ishigh: false };
 
             if (state.prev) {
                 var deltaT = (msg.timestamp - state.prev);
@@ -19,17 +15,13 @@ module.exports = function (RED) {
                     state.on += deltaT;
                 }
                 state.ishigh = msg.payload >= node.threshold;
-                state.prev = msg.timestamp;
-                context.set('state', state);
-
                 msg.payload = state.on / state.total;
-                msg.topic = 'utilization';
-
-                node.send(msg);
-            } else {
-                state.prev = msg.timestamp;
-                context.set('state', state);
+                msg.topic = node.name || 'utilization';
             }
+
+            state.prev = msg.timestamp;
+            context.set('state', state);
+            node.send(msg);
         });
     }
     RED.nodes.registerType('utilization', Utilization);
