e080a102d37fc10b53a66dfe139e87110e68d6fe
Dennis Dunn <ansofive@gmail.com>
2018-04-22 14:53:12 -0400

    The utilization reset seems to be working.

--- src/nodes/utilization.js
+++ src/nodes/utilization.js
@@ -1,27 +1,31 @@
 module.exports = function (RED) {
+    "use strict";
+
     function Utilization(config) {
         RED.nodes.createNode(this, config);
-        this.cfg = config;
+        this.threshold = Number(config.threshold);
         var node = this;
-        var initialState = { prev: 0, total: 1, on: 0, ishigh: false };
+        var state = { prev: -1, total: 1, on: 0, ishigh: false };
+
         node.on('input', msg => {
-            var context = node.context();
-            if (msg.reset) {
-                context.set('state', initialState);
-                return null;
+            var t = RED.util.getMessageProperty(msg, 'timestamp');
+            var v = RED.util.getMessageProperty(msg, 'payload');
+            t = Number(t); v = Number(v);
+            if (msg.hasOwnProperty('reset')) {
+                state = Object.assign({}, { prev: -1, total: 1, on: 0, ishigh: false });
+            } else if (!isNaN(t) && !isNaN(v)) {
+                if (state.prev >= 0) {
+                    var deltaT = (t - state.prev);
+                    state.total += deltaT;
+                    if (state.ishigh) {
+                        state.on += deltaT;
+                    }
+                    state.ishigh = (v >= node.threshold);
+                    RED.util.setMessageProperty(msg, 'payload', state.on / state.total);
+                    node.send(msg);
+                }
+                state.prev = t;
             }
-            var state = context.get('state') || initialState;
-            var deltaT = (msg.timestamp - state.prev);
-            state.total += deltaT;
-            if (state.ishigh) {
-                state.on += deltaT;
-            }
-            state.ishigh = msg.payload >= node.cfg.threshold;
-            msg.payload = state.on / state.total;
-            msg.topic = node.cfg.name;
-            state.prev = msg.timestamp;
-            context.set('state', state);
-            node.send(msg);
         });
     }
     RED.nodes.registerType('utilization', Utilization);
