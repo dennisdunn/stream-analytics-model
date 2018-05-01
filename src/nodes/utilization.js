3a85394c516d7f078a32463c50ef26b3f5b80f74
Dennis Dunn <ansofive@gmail.com>
2018-04-30 20:20:19 -0400

    Adding runtime controls.

--- src/nodes/utilization.js
+++ src/nodes/utilization.js
@@ -8,12 +8,14 @@ module.exports = function (RED) {
         var state = { prev: -1, total: 1, on: 0, ishigh: false };
 
         node.on('input', msg => {
+            var global = node.context().global;
             var t = RED.util.getMessageProperty(msg, 'timestamp');
             var v = RED.util.getMessageProperty(msg, 'payload');
             t = Number(t); v = Number(v);
             if (msg.hasOwnProperty('reset')) {
                 state = Object.assign({}, { prev: -1, total: 1, on: 0, ishigh: false });
             } else if (!isNaN(t) && !isNaN(v)) {
+                node.threshold = global.get('utilization_threshold') || node.threshold;
                 if (state.prev >= 0) {
                     var deltaT = (t - state.prev);
                     state.total += deltaT;
